2e8fb8f2ed0592fc326d3332c5516b609db8ace4
forever2k <forever2k@yandex.ru>
2021-04-13 21:49:36 +0300

    my_commit

--- app.py
+++ app.py
@@ -12,22 +12,22 @@ import aioschedule
 from aiogram.utils.exceptions import BotBlocked
 
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-sh-usage')
-
-driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
-driver.implicitly_wait(4)
-
-URL = os.getenv('URL')
-
-launch = True
-
-test_group = -1001153348142
-test = -1001364950026
-me = os.getenv('me')
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-sh-usage')
+#
+# driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+# driver.implicitly_wait(4)
+#
+# URL = os.getenv('URL')
+#
+# launch = True
+#
+# test_group = -1001153348142
+# test = -1001364950026
+# me = os.getenv('me')
 
 
 
