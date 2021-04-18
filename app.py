679934f9dccd0f86b7599fad70084e5c0d1802bf
forever2k <forever2k@yandex.ru>
2021-04-18 20:47:22 +0300

    update

--- app.py
+++ app.py
@@ -97,7 +97,6 @@ async def wake_up():
         # driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
         # driver.implicitly_wait(4)
 
-
         await bot.send_message(test_group, "Function Wake_up starts")
 
         driver.get(URL)
